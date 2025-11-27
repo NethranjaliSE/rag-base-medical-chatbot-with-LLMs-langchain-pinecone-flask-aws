6a0f695773749fdea7041058496e5b3ec41194d0
Sachini <nethranjalise@gmail.com>
2025-11-27 18:49:38 +0530

    requirement setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sachini P Nethranjali",
+    author_email="nethranjalise@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
